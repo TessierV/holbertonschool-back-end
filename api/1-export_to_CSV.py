21738a0e3ab4bae82368a473d323991ca7e82738
TessierV <5628@holbertonstudents.com>
2023-04-04 11:27:25 -0700

    not possible whitout the know of the path

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
+Write a Python script that, using this extract_employeeT API,
 for a given employee ID, returns information about
-his/her TODO list progress
+his/her TODO list progextract_employees
 export data in the CSV format.
 """
 import csv
@@ -11,15 +11,17 @@ import sys
 
 
 if __name__ == '__main__':
-    id_c = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
-    res = requests.get(url_user).json()
-    username = res.get("username")
-    req = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' +
-        (id_c) + '/todos')
+    """ EXTRACT USER DATA """
+    emp_id = sys.argv[1]
+    emp_url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    extract_employee = requests.get(emp_url).json()
+    EMPLOYEE_NAME = extract_employee.get("username")
+
+    """ EXTRACT TASK """
+    extract_employee = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(emp_id) + '/todos')
     with open("{}.csv".format(sys.argv[1]), "w") as file_c:
         writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            writer.writerow([id_c, username,
+        for task in extract_employee.json():
+            writer.writerow([emp_id, EMPLOYEE_NAME,
                             task.get("completed"), task.get("title")])
