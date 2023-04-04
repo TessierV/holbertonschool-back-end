a8eeee512ed51f302150487f258ee20a6e4809cf
TessierV <5628@holbertonstudents.com>
2023-04-04 11:17:05 -0700

    test

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,35 +1,25 @@
 #!/usr/bin/python3
 """
-Write a Python script that,
-using this REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress
+export data in the CSV format.
 """
 import csv
 import requests
 import sys
 
+
 if __name__ == '__main__':
-    # Extract employee data from the API
-    emp_id = sys.argv[1]
-    emp_url = f"https://jsonplaceholder.typicode.com/users/{emp_id}"
-    with requests.get(emp_url) as response:
-        extract_employee = response.json()
-    EMPLOYEE_NAME = extract_employee.get('name')
-
-    # Extract tasks from the API and write to CSV file
-    task_url = f"https://jsonplaceholder.typicode.com/todos/?userId={emp_id}"
-    with requests.get(task_url) as response:
-        extract_task = response.json()
-    with open(f"{emp_id}.csv", mode='w') as fil:
-        field = ['emp_id', 'employee_name', 'completed', 'title']
-        writer = csv.DictWriter(fil, fieldnames=field, quoting=csv.QUOTE_ALL)
-        writer.writeheader()
-
-        for task in extract_task:
-            if task.get('userId') == int(emp_id):
-                writer.writerow({
-                    'emp_id': emp_id,
-                    'employee_name': EMPLOYEE_NAME,
-                    'completed': task.get('completed'),
-                    'title': task.get('title')
-                })
+    id_c = sys.argv[1]
+    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
+    res = requests.get(url_user).json()
+    username = res.get("username")
+    req = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' +
+        (id_c) + '/todos')
+    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
+        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
+        for task in req.json():
+            writer.writerow([id_c, username,
+                            task.get("completed"), task.get("title")])
