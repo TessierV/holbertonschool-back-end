0553c0e7ee3b590ba03a4c6b80440f825ee83d31
TessierV <5628@holbertonstudents.com>
2023-04-05 00:22:11 -0700

    task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Write a Python script that,
+using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    """API"""
+    """ EXTRACT USER DATA """
+    emp_id = sys.argv[1]
+    emp_url = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
+    extract_employee = requests.get(emp_url).json()
+
+    """ FORMAT """
+    EMPLOYEE_USERNAME = extract_employee.get('username')
+
+    """ EXTRACT TASK """
+    task_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(emp_id)
+    extract_task = requests.get(task_url).json()
+
+    with open("{}.json".format(emp_id), "w") as user_id:
+        json.dump({emp_id: [{
+                'task': i.get('title'),
+                'completed': i.get('completed'),
+                'username': EMPLOYEE_USERNAME
+            } for i in extract_task]}, user_id)
